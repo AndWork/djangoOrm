6389c2b034546b2e07c701cc3318582b67ae6c6a
AndWork <68861431+AndWork@users.noreply.github.com>
2023-02-10 14:12:02 -0300

    Update settings.py

--- djangoOrm/settings.py
+++ djangoOrm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')(6y=k@&sr^xyb2$%6-2hx1%%%_)ctg)(an70@w@y*7n@g7)1!'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
